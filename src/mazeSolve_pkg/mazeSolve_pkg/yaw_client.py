3d740f40cc9d7ce78f9fa7fd6e7835cb1ee85862
rokaia1692006 <rokaia.moh.a@gmail.com>
2026-08-18 17:30:42 +0000

    started the client 2 more functions needed

--- src/mazeSolve_pkg/mazeSolve_pkg/yaw_client.py
+++ src/mazeSolve_pkg/mazeSolve_pkg/yaw_client.py
@@ -1,7 +1,24 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
+from nav2_msgs.action import RotateAbsolute
+import math
+# TODO :
+# create feedback function 
+# create goal_Callback
 
 class YawClient(Node):
     def __init__(self):
         super().__init__('yaw_client')
+        #creating action client 
+        self.actionC = ActionClient(self,RotateAbsolute,'rotate_absolute')
+    def send(self,target):
+        #waiting for server to start
+        self.actionC.wait_for_server()
+        #make the goal msg
+        gms = RotateAbsolute.Goal()
+        gms.target_heading = math.radians(target)
+        # send goal
+        self.send_future = self.actionC.send_goal_async(gms,feedback_callback = self.feedback)
+        # check if server accepted
+        self.send_future.add_done_callback(self.goal_callback)
