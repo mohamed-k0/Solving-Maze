e11f2508a193df07ed2c85b1c9bff943803b0468
rokaia1692006 <rokaia.moh.a@gmail.com>
2026-08-18 18:29:16 +0000

    i think im done

--- src/mazeSolve_pkg/mazeSolve_pkg/yaw_client.py
+++ src/mazeSolve_pkg/mazeSolve_pkg/yaw_client.py
@@ -1,8 +1,9 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
-from nav2_msgs.action import RotateAbsolute
 import math
+
+from interfaces.action import Move
 # TODO :
 # create feedback function 
 # create goal_Callback
@@ -11,21 +12,45 @@ class YawClient(Node):
     def __init__(self):
         super().__init__('yaw_client')
         #creating action client 
-        self.actionC = ActionClient(self,RotateAbsolute,'rotate_absolute')
+        self.actionC = ActionClient(self,Move,'rotate_absolute')
     def send(self,target):
         #waiting for server to start
-        self.actionC.wait_for_server()
+        self.get_logger().info("waiting")
+        self.actionC.wait_for_server(timeout_sec = 1.5)
         #make the goal msg
-        gms = RotateAbsolute.Goal()
-        gms.target_heading = math.radians(target)
+        gms = Move.Goal()
+        gms.turn_angle = math.radians(target)
+        gms.forward_distance = 0.0
+        gms.right_distance = 0.0
         # send goal
-        self.send_future = self.actionC.send_goal_async(gms,feedback_callback = self.feedback)
+        self.send_goal_future = self.actionC.send_goal_async(gms,feedback_callback = self.feedback)
         # check if server accepted
-        self.send_future.add_done_callback(self.goal_callback)
+        self.send_goal_future.add_done_callback(self.goal_callback)
     def feedback(self , fmsg):
         # get the feedback msg
         f = fmsg.feedback
-        # how many degrees ledt
-        left_to_turn = math.degres(f.remaining)
         # print it 
-        self.get_logger().info(f'left to turn = {left_to_turn}')
+        self.get_logger().info(f'feedback = {f.current_action,f.progress}')
+    def goal_callback(self ,future):
+        # got result 
+        goal = future.result()
+        if not goal.accepted:
+            # server didnt accept
+            self.get_logger().info("not accepted")
+            return
+        else:
+            self.get_logger().info('accepted')
+            return 
+    def result_callback(self ,future):
+        result  = future.result()
+        if result.success:
+            self.get_logger().info(f"success {result.message}")
+        else:
+            self.get_logger.error(result.message)
+def main():
+    rcply.init()
+    node = YawClient()
+    node.send()
+    rcply.spin(node)
+    node.destroy_node(node)
+    rcply.shutdown()
